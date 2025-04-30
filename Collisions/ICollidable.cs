c2306c21dfcd0151fdf161ea51cf7ca52f48c6e1
SimonRNielsen <simonrn@gmail.com>
2025-04-30 17:52:35 +0200

    Code cleanup and further optimization

--- Collisions/ICollidable.cs
+++ Collisions/ICollidable.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 
 namespace Template.Collisions
@@ -8,25 +9,57 @@ namespace Template.Collisions
     public interface ICollidable
     {
 
-
+        /// <summary>
+        /// Used as Tag (Usable in OnCollision)
+        /// </summary>
         public Enum Type { get; }
 
-
+        /// <summary>
+        /// Singular rectangle for checking intersections (collision) with other objects
+        /// </summary>
         public Rectangle CollisionBox { get; }
 
+        /// <summary>
+        /// Set to => position/Vector2 IPPCollidable.Position => Position if not using { get; set; }
+        /// </summary>
+        public Vector2 Position { get; set; }
 
-        public bool CheckCollision(ICollidable other)
+        /// <summary>
+        /// Set to => sprite/Texture2D IPPCollidable.Sprite => Sprite if not using { get; }
+        /// </summary>
+        public Texture2D Sprite { get; }
+
+        /// <summary>
+        /// Checks collision with another collidable object
+        /// </summary>
+        /// <param name="other">Other object to check collision against</param>
+        /// <returns>True if colliding</returns>
+        public bool CheckCollision(ICollidable other) => CollisionBox.Intersects(other.CollisionBox);
+
+        /// <summary>
+        /// Used to do a "collision effect" if CheckCollision returns true
+        /// </summary>
+        /// <param name="other">Effect applied on other or as effect of colliding with other.Type</param>
+        public void OnCollision(ICollidable other);
+
+        /// <summary>
+        /// Checks for proximity to other collidable objects (can be used before collision checks) to avoid some needless calculations
+        /// </summary>
+        /// <param name="other">Other objects to determine distance to</param>
+        /// <param name="buffer">Buffer radius to add to calculations</param>
+        /// <returns>True if other object is nearby</returns>
+        public bool IsNear(ICollidable other, float buffer)
         {
 
-            if (CollisionBox.Intersects(other.CollisionBox))
-                return true;
-            else
-                return false;
+            float boundingRadius = MathF.Max(Sprite.Width, Sprite.Height) / 2f;
+            float othersBoundingRadius = MathF.Max(other.Sprite.Width, other.Sprite.Height) / 2f;
+
+            float combinedRadius = boundingRadius + othersBoundingRadius + buffer;
+            float combinedRadiusSq = combinedRadius * combinedRadius;
+
+            return Vector2.DistanceSquared(Position, other.Position) <= combinedRadiusSq;
 
         }
-
-
-        public void OnCollision(ICollidable other);
 
 
     }
--- Collisions/IPPCollidable.cs
+++ Collisions/IPPCollidable.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System;
 
 namespace Template.Collisions
 {
@@ -9,13 +11,31 @@ namespace Template.Collisions
     public interface IPPCollidable
     {
 
+        /// <summary>
+        /// Used as Tag (Usable in OnCollision)
+        /// </summary>
+        public Enum Type { get; }
 
-        public Vector2 ColliderPosition { get; }
+        /// <summary>
+        /// Set to => position/Vector2 IPPCollidable.Position => Position if not using { get; set; }
+        /// </summary>
+        public Vector2 Position { get; set; }
 
+        /// <summary>
+        /// Set to => sprite/Texture2D IPPCollidable.Sprite => Sprite if not using { get; }
+        /// </summary>
+        public Texture2D Sprite { get; }
 
+        /// <summary>
+        /// Instantiate after setting sprite (can be done in constuctor if sprite is provided/provided in base constructor), by doing "CreateRectangels()"
+        /// </summary>
         public List<RectangleData> Rectangles { get; set; }
 
-
+        /// <summary>
+        /// Used to check collision against object that doesn't use pixel perfect collision
+        /// </summary>
+        /// <param name="other">Rectangle/CollisionBox to perform check against</param>
+        /// <returns>True if collision detected</returns>
         public bool DoHybridCheck(Rectangle other)
         {
 
@@ -27,40 +47,60 @@ namespace Template.Collisions
 
         }
 
-
+        /// <summary>
+        /// Used to check collision against object that also uses pixel perfect collision 
+        /// </summary>
+        /// <param name="other">Other object to check collisions against</param>
+        /// <returns>True if collision detected</returns>
         public bool PPCheckCollision(IPPCollidable other)
         {
 
-            foreach (RectangleData rect1 in Rectangles)
-                foreach (RectangleData rect2 in other.Rectangles)
-                    if (rect1.Rectangle.Intersects(rect2.Rectangle))
-                        return true;
+            if (Rectangles != null && other.Rectangles != null)
+                foreach (RectangleData rect1 in Rectangles)
+                    foreach (RectangleData rect2 in other.Rectangles)
+                        if (rect1.Rectangle.Intersects(rect2.Rectangle))
+                            return true;
 
             return false;
 
         }
 
-
-        public void UpdateRectangles(int width, int height)
+        /// <summary>
+        /// Updates position for collision rectangles, must be run regularly at minimum
+        /// </summary>
+        public void UpdateRectangles()
         {
-            foreach (RectangleData rectangle in Rectangles)
-                rectangle.UpdatePosition(ColliderPosition, width, height);
+
+            if (Rectangles == null || Rectangles.Count == 0)
+                CreateRectangles();
+
+            if (Rectangles != null)
+                foreach (RectangleData rectangle in Rectangles)
+                    rectangle.UpdatePosition(Position, Sprite.Width, Sprite.Height);
+
         }
 
-
-        public List<RectangleData> CreateRectangles(Texture2D sprite)
+        /// <summary>
+        /// Creates a List of Rectangles for collision checks (is automatically run on UpdateRectangles if Rectangles count is 0 or it's null
+        /// </summary>
+        /// <returns>List used to instantiate "Rectangles"</returns>
+        /// <exception cref="Exception">Throws exception if no sprite was found</exception>
+        public List<RectangleData> CreateRectangles()
         {
+
+            if (Sprite == null)
+                throw new Exception($"No sprite set to do \"CreateRectangles()\" method on for {Type.ToString()}");
 
             List<RectangleData> rectangleList = new List<RectangleData>();
             List<Color[]> lines = new List<Color[]>();
 
-            for (int i = 0; i < sprite.Height; i++)
+            for (int i = 0; i < Sprite.Height; i++)
             {
 
-                Color[] colors = new Color[sprite.Width];
-                sprite.GetData(0, new Rectangle(0, i,
-                    sprite.Width, 1), colors, 0,
-                    sprite.Width);
+                Color[] colors = new Color[Sprite.Width];
+                Sprite.GetData(0, new Rectangle(0, i,
+                    Sprite.Width, 1), colors, 0,
+                    Sprite.Width);
                 lines.Add(colors);
 
             }
@@ -96,13 +136,16 @@ namespace Template.Collisions
 
     }
 
+    /// <summary>
+    /// Logic class for Pixel Perfect collision
+    /// </summary>
     public class RectangleData
     {
 
         public Rectangle Rectangle { get; set; } = new Rectangle();
 
 
-        public int X { get; set; } 
+        public int X { get; set; }
 
 
         public int Y { get; set; }
@@ -116,7 +159,12 @@ namespace Template.Collisions
 
         }
 
-
+        /// <summary>
+        /// Used to update Rectangle(s)
+        /// </summary>
+        /// <param name="position">Objects position</param>
+        /// <param name="width">Sprite width</param>
+        /// <param name="height">Sprite height</param>
         public void UpdatePosition(Vector2 position, int width, int height)
         {
 
