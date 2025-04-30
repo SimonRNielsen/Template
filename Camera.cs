8b46fd0ac8877bde2efcdaf263bf7c965c88fcdf
SimonRNielsen <simonrn@gmail.com>
2025-04-30 15:44:26 +0200

    Added a Camera-class with working mouse position converter

---
+++ Camera.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Template
+{
+    public class Camera
+    {
+
+        #region Fields
+
+        private Vector2 position;
+        private readonly GraphicsDevice _graphicsDevice;
+
+        #endregion
+        #region Properties
+
+
+        /// <summary>
+        /// Property to access the posi
+        /// </summary>
+        public Vector2 Position
+        {
+
+            get => position;
+
+            set
+            {
+
+                position = value;
+
+            }
+
+        }
+
+        /// <summary>
+        /// Used to set the zoomlevel of the viewport (scaled float)
+        /// </summary>
+        public float Zoom { get; set; }
+
+        /// <summary>
+        /// Used to rotate the camera
+        /// </summary>
+        public float Rotation { get; set; }
+
+        #endregion
+        #region Constuctor
+
+        /// <summary>
+        /// Constructor for the camera viewport
+        /// </summary>
+        /// <param name="graphicsDevice">Defines which graphicsdevice to get viewport parameters from</param>
+        /// <param name="position">Defines starting position of the viewport</param>
+        public Camera(GraphicsDevice graphicsDevice)
+        {
+            _graphicsDevice = graphicsDevice;
+            Zoom = 1f;
+            Rotation = 0.0f;
+        }
+
+        #endregion
+        #region Methods
+
+        /// <summary>
+        /// Transforms the cameraviewport to return value
+        /// </summary>
+        /// <returns>Center of screen location</returns>
+        public Matrix GetTransformation()
+        {
+            var screenCenter = new Vector3(_graphicsDevice.Viewport.Width / 2f, _graphicsDevice.Viewport.Height / 2f, 0);
+
+            return Matrix.CreateTranslation(-Position.X, -Position.Y, 0) *
+                   Matrix.CreateRotationZ(Rotation) *
+                   Matrix.CreateScale(Zoom, Zoom, 1) *
+                   Matrix.CreateTranslation(screenCenter);
+        }
+
+        /// <summary>
+        /// Reverts transformation to get position compared to viewport
+        /// </summary>
+        /// <returns>Inverted "GetTransformation" Matrix</returns>
+        public Matrix InverseTransformation()
+        {
+
+            return Matrix.Invert(GetTransformation());
+
+        }
+
+        /// <summary>
+        /// Converts a position on the viewport (mouse position as example) into ingame position
+        /// </summary>
+        /// <param name="position">The Vector2 to be transformed</param>
+        /// <returns>Useable relative position</returns>
+        public Vector2 RefactorPosition(Vector2 position)
+        {
+
+            return Vector2.Transform(position, InverseTransformation());
+
+        }
+
+        #endregion
+
+    }
+}
